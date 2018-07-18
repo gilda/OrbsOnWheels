e7b41c10e6e5a7f84d6f4f971ae9cfce5167dccc
Gilda <gil@daganmail.com>
2018-07-18 10:27:52 +0300

    18/07/18 triple car simulation

--- Simulation/main.py
+++ Simulation/main.py
@@ -6,21 +6,31 @@ import math
 # other files
 from car import *
 
-
 # window size for plotting
 WINDOW_SIZE = 1
 # frames per second
-FPS = 30
+FPS = 20
 
 # commands for cars
 cmd1 = ""
 cmd2 = ""
+cmd3 = ""
+
+cmd1Input = ["WAIT 100", "ROT 30", "MOVXY 0.5 0.3", "WAIT 30", "ROT 180"]
+cmd1Index = 0
+
+cmd2Input = ["ROT 30", "MOVXY 0.8 0.8", "MOVXY 0.2 0.3", "WAIT 30"]
+cmd2Index = 0
+
+cmd3Input = ["RAD 0.2 -90", "MOVXY 0.8 0.6", "WAIT 30", "ROT 180"]
+cmd3Index = 0
 
 
 def iterate(i, *fargs):
     # get patches for drawing
     car1 = fargs[0]
     car2 = fargs[1]
+    car3 = fargs[2]
 
     # set the cars velocity
     car1.setVelocity(0.005)
@@ -28,13 +38,24 @@ def iterate(i, *fargs):
     # using global variable to keep track of commands between loops
     global cmd1
     global cmd2
+    global cmd3
+
+    global cmd1Index
+    global cmd1Input
+    global cmd2Index
+    global cmd2Input
+    global cmd3Index
+    global cmd3Input
 
     # change and ask for new state only if the current state is stop
     # meaning that all current commands were accomplished
     if car1.state == None or car1.state == car1.stop:
         # get new command
-        cmd1 = input("cmd1: ")
-        cmd1 = cmd1.split(" ")
+        #cmd1 = input("cmd1: ")
+        if cmd1Index < len(cmd1Input) - 1:
+            cmd1 = cmd1Input[cmd1Index]
+            cmd1Index += 1
+            cmd1 = cmd1.split(" ")
         if cmd1[0] == "ROT":
             car1.rotate(int(cmd1[1]))
         elif cmd1[0] == "MOV":
@@ -73,8 +94,11 @@ def iterate(i, *fargs):
     # meaning that ll current commands were accomplished
     if car2.state == None or car2.state == car2.stop:
         # get new command
-        cmd2 = input("cmd2: ")
-        cmd2 = cmd2.split(" ")
+        #cmd2 = input("cmd2: ")
+        if cmd2Index < len(cmd2Input) - 1:
+            cmd2 = cmd2Input[cmd2Index]
+            cmd2Index += 1
+            cmd2 = cmd2.split(" ")
         if cmd2[0] == "ROT":
             car2.rotate(int(cmd2[1]))
         elif cmd2[0] == "MOV":
@@ -102,10 +126,54 @@ def iterate(i, *fargs):
         else:
             print("No such command")
 
+    car2.decInterval()
     car2.draw()
 
+
+    # set the second cars velocity
+    car3.setVelocity(0.005)
+
+    # change and ask for new state only if the current state is stop
+    # meaning that ll current commands were accomplished
+    if car3.state == None or car3.state == car3.stop:
+        # get new command
+        #cmd3 = input("cmd3: ")
+        if cmd3Index < len(cmd3Input) - 1:
+            cmd3 = cmd3Input[cmd3Index]
+            cmd3Index += 1
+            cmd3 = cmd3.split(" ")
+        if cmd3[0] == "ROT":
+            car3.rotate(int(cmd3[1]))
+        elif cmd3[0] == "MOV":
+            car3.move()
+        elif cmd3[0] == "MOVXY":
+            car3.move_xy(float(cmd3[1]), float(cmd3[2]))
+        elif cmd3[0] == "RAD":
+            car3.move_rad(float(cmd3[1]), float(cmd3[2]))
+        elif cmd3[0] == "WAIT":
+            car3.wait(int(cmd3[1]))
+        else:
+            print("No such command")
+    else:
+        # continue with current command
+        if cmd3[0] == "ROT":
+            car3.rotate(int(cmd3[1]))
+        elif cmd3[0] == "MOV":
+            car3.move()
+        elif cmd3[0] == "MOVXY":
+            car3.move_xy(float(cmd3[1]), float(cmd3[2]))
+        elif cmd3[0] == "RAD":
+            car3.move_rad(float(cmd3[1]), float(cmd3[2]))
+        elif cmd3[0] == "WAIT":
+            car3.decInterval()
+        else:
+            print("No such command")
+
+    car3.decInterval()
+    car3.draw()
+
     # return cars for drawing
-    return car1.patch, car2.patch
+    return car1.patch, car2.patch, car3.patch
 
 
 def main():
@@ -119,17 +187,20 @@ def main():
 
     # create car patches for drawing on plot
     car1 = Car(0, 0.5, 0.5, WINDOW_SIZE / 20, plt.Polygon(calcTriangle(0,
-                                                                    WINDOW_SIZE / 20), closed=True, facecolor="red"))
+                                                                       WINDOW_SIZE / 20), closed=True, facecolor="red"))
     car2 = Car(1, 0.4, 0.4, WINDOW_SIZE / 20, plt.Polygon(calcTriangle(0,
-                                                                    WINDOW_SIZE / 20), closed=True, facecolor="blue"))
+                                                                       WINDOW_SIZE / 20), closed=True, facecolor="blue"))
+    car3 = Car(2, 0.2, 0.6, WINDOW_SIZE / 20, plt.Polygon(calcTriangle(0,
+                                                                       WINDOW_SIZE / 20), closed=True, facecolor="green"))
 
     # add patches to the drawing
     ax.add_patch(car1.patch)
     ax.add_patch(car2.patch)
+    ax.add_patch(car3.patch)
 
     # main animation update function
     anim = matplotlib.animation.FuncAnimation(
-        fig, iterate, fargs=[car1, car2], interval=1000 / FPS)
+        fig, iterate, fargs=[car1, car2, car3], interval=1000 / FPS)
     plt.show()
 
 
